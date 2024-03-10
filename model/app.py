7c207d432dabdb361813293d91925fa62c239e7a
Prathamesh Kokate <132193868+prkokate@users.noreply.github.com>
2024-03-10 15:13:07 +0530

    Model changed, with two way prediction and closest result effect added for optimized results

--- model/app.py
+++ model/app.py
@@ -22,19 +22,26 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 
-rf=RandomForestClassifier(n_estimators=50,min_samples_split=10,random_state=1)
+rf=RandomForestClassifier(n_estimators=30,min_samples_split=15,random_state=137)
 
-# class MissingDict(dict):
-#     __missing__ = lambda self, key: key
+class MissingDict(dict):
+    __missing__ = lambda self, key: key
 
-# map_values = {"Brighton and Hove Albion": "Brighton",
-#               "Manchester United": "Manchester Utd",
-#               "Newcastle United": "Newcastle Utd", 
-#               "Tottenham Hotspur": "Tottenham", 
-#               "West Ham United": "West Ham", 
-#               "Wolverhampton Wanderers": "Wolves"
-#              } 
-# mapping = MissingDict(**map_values)
+map_values = {"Brighton and Hove Albion": "Brighton",
+              "Manchester Utd": "Manchester United",
+              "Newcastle Utd": "Newcastle United", 
+              "Tottenham Hotspur": "Tottenham", 
+              "West Ham United": "West Ham", 
+              "Wolverhampton Wanderers": "Wolves",
+              "Wolverhampton Wanderer": "Wolves",
+              "Nott'ham Forest":"Nottingham",
+              "Nottingham Fore":"Nottingham",
+              "heffield United":"Sheffield United",
+              "Sheffield Utd":"Sheffield United",
+              "Chelse":"Chelsea",
+              "Asron Vill":"Aston Villa"
+             } 
+mapping = MissingDict(**map_values)
 
 # Setup url route which will calculate 
 # total sum of array. 
@@ -56,12 +63,14 @@ def TrainModel(data):
     matches=pd.read_csv('f:/Machine Learning/MatchPredictor/model/matches.csv',index_col=0)
     
     matches=matches.groupby("team").get_group(data["team"])
+    matches["target"]=matches["result"].astype("category").cat.codes
     new_row=pd.DataFrame(data,index=[0])
     matches = pd.concat([matches,new_row], ignore_index = True)
     
     matches["date"]=pd.to_datetime(matches["date"])
     matches["opp_code"]=matches["opponent"].astype("category").cat.codes
     matches["hour"]=matches["time"].str.replace(":.+","",regex=True).astype("int")
+    matches["formation"].ffill(inplace=True)
     matches["formation_code"]=matches["formation"].str.replace(r'\D',"",regex=True).astype("int")
     matches["day_code"]=matches["date"].dt.dayofweek
     matches["venue_code"]=matches["venue"].astype("category").cat.codes
@@ -75,43 +84,72 @@ def TrainModel(data):
     
     
     matches_rolling=rolling_averages(matches,cols,new_cols)
+    special=matches_rolling.groupby("opponent").get_group(data["opponent"])
+    if(special.shape[0]>=2):
+        matches_rolling=special
+    matches_rolling.index=[i for i in range(matches_rolling.shape[0])]
+    matches_rolling.sort_index(axis=0, ascending=True, inplace=False, kind='quicksort')
     last_index=matches_rolling.index.sort_values()[-1]
     # print("LATS INDEX=",matches_rolling.index[-1])
     
-    prev_matches=matches_rolling.loc[:last_index-1]
+    prev_matches=matches_rolling.loc[:matches_rolling.shape[0]-2]
     
     train=prev_matches
     rf.fit(train[predictors+new_cols],train['target'])
-    return rf, matches_rolling.loc[last_index:]
+    return rf, matches_rolling.loc[matches_rolling.shape[0]-1:]
 
 
 @app.route('/predict', methods = ['POST']) 
 @cross_origin()
 def predict_match(): 
 
-    #Request Body Data:
-    data = request.get_json()
-    
-
-    model,test_data=TrainModel(data)
-    cols=["gf","ga","sh","sot","dist","fk","pk","pkatt"]
-    new_cols=[f"{c}_rolling" for c in cols]
-    predictors=["venue_code","opp_code","hour","day_code",'formation_code']
+        #Request Body Data:
+        data = request.get_json()
 
 
-    result=model.predict(test_data[predictors+new_cols])[0]
+        model,test_data=TrainModel(data)
+
+        cols=["gf","ga","sh","sot","dist","fk","pk","pkatt"]
+        new_cols=[f"{c}_rolling" for c in cols]
+        predictors=["venue_code","opp_code","hour","day_code",'formation_code']
+
+
+        result=model.predict(test_data[predictors+new_cols]).astype(int).tolist()[0]
+
+        data2=data
+        team=data["team"]
+        data2["team"]=data["opponent"]
+        data2["opponent"]=team
+        data2["formation"]=np.nan
+        if(data["venue"]=="Home"):
+            data2["venue"]="Away"
+        else:
+            data2["venue"]="Home"
+
+
+        model2,test_data2=TrainModel(data2)
+
+        result2=model2.predict(test_data2[predictors+new_cols]).astype(int).tolist()[0]
+
+        finalresult=result
+        if(result==result2):
+            finalresult=0
+        elif(result==2.0 and result==1.0):
+            finalresult=2
+        elif(result==1.0 and result2==2.0):
+            finalresult=1
+        elif(result==0.0 and result2==1.0):
+            finalresult=2
+        elif(result==0.0 and result2==2.0):
+            finalresult=1
 
 
 
-    print("result=",result)
-    res=0
-
-    if(result==1):
-        res=1
+        print("result=",finalresult)
 
 
-    # Return data in json format 
-    return json.dumps({"result":res}) 
+        # Return data in json format 
+        return json.dumps({"result":finalresult}) 
 
 @app.route('/schedule', methods = ['GET']) 
 @cross_origin()
@@ -161,6 +199,8 @@ def get_schedule():
         schmatches.append(scheduled)
     scheduled_matches=pd.concat(schmatches)
     scheduled_matches=scheduled_matches.drop(["gf","ga","xg","xga","attendance","captain","referee","match report","notes"],axis=1)
+    scheduled_matches["team"]=scheduled_matches["team"].map(mapping)
+    scheduled_matches["opponent"]=scheduled_matches["opponent"].map(mapping)
     scheduled_matches.index=[i for i in range(0,len(scheduled_matches.index))]
     response=scheduled_matches.to_json(index=False,date_format="iso",orient='records')
     return json.dumps(response)
